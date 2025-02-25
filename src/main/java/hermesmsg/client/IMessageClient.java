2a5cd3773ce94d30ca32faee407a31eb089d65e2
Sungwon Um <shineum@gmail.com>
2025-02-24 21:56:02 -0800

    update interface

--- src/main/java/hermesmsg/client/IMessageClient.java
+++ src/main/java/hermesmsg/client/IMessageClient.java
@@ -1,9 +1,12 @@
 package hermesmsg.client;
 
-import jakarta.mail.Message;
-
+import java.util.Properties;
 import java.util.logging.Logger;
 
 public interface IMessageClient {
     Logger logger = Logger.getLogger(IMessageClient.class.getName());
+
+    IMessageClient initClient(Properties props);
+
+    void send(String msg);
 }
