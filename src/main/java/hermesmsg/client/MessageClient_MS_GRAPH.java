e3426884dec05d1fa83bc6a2bf14bf5ede499d93
Sungwon Um <shineum@gmail.com>
2025-02-24 21:57:34 -0800

    add message clients

---
+++ src/main/java/hermesmsg/client/MessageClient_MS_GRAPH.java
@@ -0,0 +1,16 @@
+package hermesmsg.client;
+
+import java.util.Properties;
+
+public class MessageClient_MS_GRAPH implements IMessageClient {
+    @Override
+    public IMessageClient initClient(Properties props) {
+        // TODO implement initClient
+        return this;
+    }
+
+    @Override
+    public void send(String msg) {
+        // TODO send
+    }
+}
---
+++ src/main/java/hermesmsg/client/MessageClient_SMTP.java
@@ -0,0 +1,16 @@
+package hermesmsg.client;
+
+import java.util.Properties;
+
+public class MessageClient_SMTP implements IMessageClient {
+    @Override
+    public IMessageClient initClient(Properties props) {
+        // TODO implement initClient
+        return this;
+    }
+
+    @Override
+    public void send(String msg) {
+        // TODO send
+    }
+}
