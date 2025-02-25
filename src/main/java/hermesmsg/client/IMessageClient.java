099c9161ddf57dc647ee1c1f0e0f496cc96f1cf8
Sungwon Um <shineum@gmail.com>
2025-02-24 20:41:04 -0800

    add logger

--- src/main/java/hermesmsg/client/IMessageClient.java
+++ src/main/java/hermesmsg/client/IMessageClient.java
@@ -2,5 +2,8 @@ package hermesmsg.client;
 
 import jakarta.mail.Message;
 
+import java.util.logging.Logger;
+
 public interface IMessageClient {
+    Logger logger = Logger.getLogger(IMessageClient.class.getName());
 }
--- src/main/java/hermesmsg/handler/IMessageQueueHandler.java
+++ src/main/java/hermesmsg/handler/IMessageQueueHandler.java
@@ -1,7 +1,10 @@
 package hermesmsg.handler;
 
 import java.util.List;
+import java.util.logging.Logger;
 
 public interface IMessageQueueHandler {
+    Logger logger = Logger.getLogger(IMessageQueueHandler.class.getName());
+
     public void addMessage(String jsonMsgStr);
 }
