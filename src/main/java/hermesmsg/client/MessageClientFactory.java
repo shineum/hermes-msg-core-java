a2986ed82a031603a44930ac509ab0c3f2306709
Sungwon Um <shineum@gmail.com>
2025-02-27 21:51:39 -0800

    update logger

--- src/main/java/hermesmsg/client/MessageClientFactory.java
+++ src/main/java/hermesmsg/client/MessageClientFactory.java
@@ -1,11 +1,16 @@
 package hermesmsg.client;
 
+import hermesmsg.entity.EmailMessage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.lang.reflect.Method;
 import java.util.Properties;
-import java.util.logging.Logger;
+//import java.util.logging.Logger;
 
 public class MessageClientFactory {
-    static Logger logger = Logger.getLogger(MessageClientFactory.class.getName());
+
+    static Logger logger = LoggerFactory.getLogger(MessageClientFactory.class);
 
     @SuppressWarnings("unchecked")
     public static IMessageClient create(String className, Properties props) {
@@ -14,7 +19,7 @@ public class MessageClientFactory {
             Method method = clazz.getDeclaredMethod("initClient", Properties.class);
             return (IMessageClient) method.invoke(clazz.getConstructor().newInstance(), props);
         } catch (Exception e) {
-            logger.severe(e.toString());
+            logger.error("[CREATE]", e);
         }
         return null;
     }
