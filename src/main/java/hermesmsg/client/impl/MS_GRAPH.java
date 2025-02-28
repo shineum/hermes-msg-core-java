f0ea218a9a2c3d7c6194471658699664695eed38
Sungwon Um <shineum@gmail.com>
2025-02-27 21:49:21 -0800

    update SMTP

--- src/main/java/hermesmsg/client/impl/MS_GRAPH.java
+++ src/main/java/hermesmsg/client/impl/MS_GRAPH.java
@@ -2,10 +2,14 @@ package hermesmsg.client.impl;
 
 import hermesmsg.client.IMessageClient;
 import org.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Properties;
 
 public class MS_GRAPH implements IMessageClient {
+    static Logger logger = LoggerFactory.getLogger(SMTP.class);
+
     @Override
     public IMessageClient initClient(Properties props) {
         // TODO implement initClient
