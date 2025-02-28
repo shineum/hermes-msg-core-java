85d7a914c9522f64d939e6fb021fb949b662aa8a
Sungwon Um <shineum@gmail.com>
2025-02-27 21:20:40 -0800

    remove logger

--- src/main/java/hermesmsg/client/IMessageClient.java
+++ src/main/java/hermesmsg/client/IMessageClient.java
@@ -3,11 +3,8 @@ package hermesmsg.client;
 import org.json.JSONObject;
 
 import java.util.Properties;
-import java.util.logging.Logger;
 
 public interface IMessageClient {
-    Logger logger = Logger.getLogger(IMessageClient.class.getName());
-
     IMessageClient initClient(Properties props);
 
     void send(String msg, JSONObject options);
