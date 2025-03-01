f6c9186252d49ca1c34a43ce5ef53cdb0211e1f8
Sungwon Um <shineum@gmail.com>
2025-02-28 23:03:59 -0800

    add constants

--- src/main/java/hermesmsg/util/Constant.java
+++ src/main/java/hermesmsg/util/Constant.java
@@ -7,10 +7,14 @@ public interface Constant {
     public static final String MESSAGE_KEY_BODY = "body";
     public static final String MESSAGE_KEY_BODY_CONTENT = "content";
     public static final String MESSAGE_KEY_BODY_CONTENT_TYPE = "contentType";
+    public static final String MESSAGE_KEY_IS_HTML = "isHtml";
     public static final String MESSAGE_KEY_FROM = "from";
-    public static final String MESSAGE_KEY_TO = "toRecipients";
-    public static final String MESSAGE_KEY_CC = "ccRecipients";
-    public static final String MESSAGE_KEY_BCC = "bccRecipients";
+    public static final String MESSAGE_KEY_TO = "to";
+    public static final String MESSAGE_KEY_CC = "cc";
+    public static final String MESSAGE_KEY_BCC = "bcc";
+    public static final String MESSAGE_KEY_TO_RECIPIENTS = "toRecipients";
+    public static final String MESSAGE_KEY_CC_RECIPIENTS = "ccRecipients";
+    public static final String MESSAGE_KEY_BCC_RECIPIENTS = "bccRecipients";
     public static final String MESSAGE_KEY_EMAIL = "emailAddress";
     public static final String MESSAGE_KEY_EMAIL_ADDRESS = "address";
     public static final String MESSAGE_KEY_EMAIL_NAME = "name";
@@ -24,6 +28,8 @@ public interface Constant {
     public static final String MESSAGE_VALUE_ATTACHMENTS_DATA_TYPE = "#microsoft.graph.fileAttachment";
     public static final String OPTION_USE_COMPRESS = "useCompress";
     public static final String TXT_CLIENT_NAME = "clientName";
-    public static final String TXT_MSG = "msg";
+    public static final String TXT_QUEUE_MSG_CONTENT = "queueMsgContent";
     public static final String TXT_OPTIONS = "options";
+    public static final String MESSAGE_CONTENT_TYPE_HTML = "text/html";
+    public static final String MESSAGE_CONTENT_TYPE_TEXT = "text/plain";
 }
